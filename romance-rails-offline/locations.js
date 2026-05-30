f84202be0360b7f61b2a6fb7f26631b7ff2b68b1
Fayrouz <semiauto1983@gmail.com>
2026-05-30 18:35:48 +0300

    Delete romance-rails-offline directory

--- romance-rails-offline/locations.js
+++
@@ -1,9 +0,0 @@
-
-const validLocations = [
-  "ikebukuro-station.html",
-  "hareza-ikebukuro.html",
-  "ikebukuro-2-chome-station.html",
-  "ikebukuro-futamata-koban-station.html",
-  "mejiro-station.html",
-  "otsuka-station.html",
-];
